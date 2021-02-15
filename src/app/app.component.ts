824c1903a8823f5f7699ed48ded947de6d4791fc
dejanalb <kelixone@tiscali.it>
2021-02-15 11:26:28 +0100

    first day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface ChatRow {
+  text: string;
+  date: string;
+}
+interface ListUserRow {
+  text: string;
+  status: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public Users: ListUserRow[]=[];
+  public chatMessages: ChatRow[]=[];
+  public message: string ;
+  public chat: string ;
+
+  public submit() {
+    let sendtime = new Date().toLocaleString();
+    let row:ChatRow={text:this.message, date:sendtime};
+    this.chatMessages.push(row);
+    }
   title = 'rjc-webchat';
-}
+} 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
